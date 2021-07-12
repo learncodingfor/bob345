b02b8759a510657c443eaa9592efcbdb800e5a2e
pytube832 <78810546+pytube832@users.noreply.github.com>
2021-07-12 12:02:21 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,3 +2,10 @@ var message:string = "hello world!"
 console.log(message);
 var num:number = 12
 console.log(num);
+class Greeting{
+  greet():void{
+    console.log("hi")
+  }
+}
+var obj = new Greeting();
+obj.greet();
