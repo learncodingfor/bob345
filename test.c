c05c6b95f56f535fb49423a8d07062794f881cb7
pytube832 <78810546+pytube832@users.noreply.github.com>
2021-07-12 16:24:32 -0400

    Update test.c

--- test.c
+++ test.c
@@ -59,5 +59,24 @@ int main(){
     FILE *fp = fopen("write.txt",'w');
     fputs("real python!");
     fclose(fp);
-    return 1;
+    
 }
+#include <python.h>
+static PyObiect *method_ftputs(PyObject *self, PyObject * args){
+    char *str, *filename = NULL;
+    int bytes_copied = -1;
+    
+    /*Parse Arguments*/
+    if(!PyArg_ParseTuple(args, "ss", &str, &filename)) {
+        return NULL;
+    }
+    FILE *fp = fopen(filename, "w");
+    bytes_copied = -1;
+    fclose(fp);
+    
+    return PyLong_FromLong(bytes_copied);
+}
+static PyMethodDef FputsMethods[] = {
+    {"ftputs", method_ftputs, METH_VARARGS, "python interface for ftputs C library functionl"},
+    {NULL, NULL, 0, NULL}
+};
