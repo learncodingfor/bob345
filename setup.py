cfef2248c82740f63d4d88a9edb2bba76382880a
pytube832 <78810546+pytube832@users.noreply.github.com>
2021-07-12 17:06:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup, Extension
 setup(name = 'myModule','version = '1.0',  \
-   ext_modules = [Extensions('myModule', ['test.c'])])
+   ext_modules = [Extensions('myModule', ["test.c"])])
