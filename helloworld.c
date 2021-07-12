93501753f2ff827cc05a2374cdc2efeedc71b705
pytube832 <78810546+pytube832@users.noreply.github.com>
2021-07-12 13:37:20 -0400

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(){
+  /*learning c programming language :)*/
+  printf("hello World!");
+  
+  return 0;
+}
+int age = 5;
+if(age < 5) {
+  printf("i am 5 years old :)");
+}
+else {
+  printf("i am", age);
+}
