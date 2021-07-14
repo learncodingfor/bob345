2e831c2aad63122683c0bb99faed2cd7e7f36144
pytube832 <78810546+pytube832@users.noreply.github.com>
2021-07-14 12:32:03 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,2 +1,2 @@
 var s = document.getElementById("s").value;
-if(s == "jojo's bizaare adventure's"){window.location.href="search.html?s=jojosbizzareadventures");}
+window.location.href ="search.html" + "?s="+s);
