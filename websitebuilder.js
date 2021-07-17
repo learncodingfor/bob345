84a06787ce27b9069a2027eedb07e0b2d9e938b5
pytube832 <78810546+pytube832@users.noreply.github.com>
2021-07-17 15:14:54 -0400

    Delete websitebuilder.js

--- websitebuilder.js
+++
@@ -1,5 +0,0 @@
-var p = document.getElementById("p").value;
-var page = prompt("page name:");
-var pa = window.location.href = "https://pytube832.github.io/wikianime.go/websitebuilder.html?page="+page;
-if(window.location.href =="https://pytube832.github.io/wikianime.go/websitebuilder.html"+pa){alert("hi")}
-
