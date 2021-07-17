5da67676349df368448dd7c16c56fd810233c4ab
pytube832 <78810546+pytube832@users.noreply.github.com>
2021-07-17 15:07:40 -0400

    Update websitebuilder.js

--- websitebuilder.js
+++ websitebuilder.js
@@ -1,3 +1,3 @@
 var p = document.getElementById("p").value;
 var page = prompt("page name:");
-window.location.href="https://pytube832.github.io/wikianime.go/websitebuilder.html?page="+p+"?index";
+window.location.href="https://pytube832.github.io/wikianime.go/websitebuilder.html?page="+page+"?index";
